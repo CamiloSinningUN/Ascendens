796e3d4d47d233e81b439d3386b22a87c249eb08
CamiloSinningUN <camilosinning.cs@gmail.com>
2020-02-29 15:33:57 -0500

    Nickname

---
+++ Ascendens/Assets/Clase_Cuenta.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clase_Cuenta : MonoBehaviour
+{
+    public string[] Nick = new string[3];
+    
+    public int[] Nivelc = new int[3];
+    public int[] dinero = new int[3];
+    public int[] idjugador = new int[3];
+    
+   
+
+
+
+}
