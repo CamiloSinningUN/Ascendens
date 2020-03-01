6019855f29d2717ffdf663c33489f880e556ea2b
CamiloSinningUN <camilosinning.cs@gmail.com>
2020-02-29 19:32:08 -0500

    Sistema de datos actualizado
    Se incluyo un sistema orientado a objetos

---
+++ Ascendens/Assets/POO/Clase_personaje.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clase_personaje : MonoBehaviour
+{
+    public int dinero;
+    public int id;
+    public int vida;
+    public int mana;
+    public int fuerza;
+    public string nick;
+    public int dificultad;
+    public int minivel;
+
+    public Clase_personaje(int dinero, int id, int vida, int mana, int fuerza, string nick, int dificultad,int minivel)
+    {
+        this.dinero = dinero;
+        this.id = id;
+        this.vida = vida;
+        this.mana = mana;
+        this.fuerza = fuerza;
+        this.nick = nick;
+        this.dificultad = dificultad;
+        this.minivel = minivel;
+    }
+}
